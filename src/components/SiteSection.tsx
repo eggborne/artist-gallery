1f0576853edb67699cfee7c95d8b0482d1d90148
eggborne <mike@mikedonovan.dev>
2024-05-07 10:51:30 -0700

    Adjust gallery style

--- src/components/SiteSection.tsx
+++ src/components/SiteSection.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './SiteSection.css'
 import classNames from 'classnames';
 
@@ -19,28 +18,15 @@ function SiteSection({ sectionData, imageSectionArray, userImages }: SiteSection
         {sectionData.id == 0 && userImages && userImages.length ?
           <div className='gallery-display'>
             {imageSectionArray.map(seriesSection =>
-              <div key={seriesSection.series} className='gallery-series'>
+              <>
                 <h2>{seriesSection.series}</h2>
-                {seriesSection.images.map(image =>
-              <img key={`${image.fileName}-thumbnail`} className='gallery-thumbnail' src={image.thumbnailUrl} alt={image.title} />
-            // (<div className='gallery-tile' key={image.fileName}>
-            // (<div  key={image.fileName}>
-            //   <img className='gallery-thumbnail' src={image.thumbnailUrl} alt={image.title} />
-            //   {/* <div className='gallery-tile-info'>
-            //     <h2>{image.title}</h2>
-            //     <div>{image.description}</div>
-            //     <div className='two-column'>
-            //       <div>{image.media}</div>
-            //       <div>{
-              //         `${image.dimensions.width}${image.dimensions.unit === 'in.' ? '"' : image.dimensions.unit} x ${image.dimensions.height}${image.dimensions.unit === 'in.' ? '"' : image.dimensions.unit}`
-              //       }</div>
-              //     </div>
-            //   </div> */}
-            // </div>
-                  // )
-                    )}
-              </div>
-          )}
+                <div key={seriesSection.series} className='gallery-series'>
+                  {seriesSection.images.map(image =>
+                    <img key={`${image.fileName}-thumbnail`} className='gallery-thumbnail' src={image.thumbnailUrl} alt={image.title} />
+                  )}
+                </div>
+              </>
+            )}
           </div>
           :
           null
